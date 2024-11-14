469bab7d91721d17dc33a8282d7a305bd502de5d
AchaUmbaPretre <achandambi@gmail.com>
2024-11-14 17:01:10 +0100

    commit

--- src/components/statistiqueItems/StatistiqueItems.jsx
+++ src/components/statistiqueItems/StatistiqueItems.jsx
@@ -7,21 +7,21 @@ const StatistiqueItems = () => {
             <div className="static_wrapper">
                 <div className="static_row">
                     <h2 className="static_h2">Title</h2>
-                    <h1 className="static_h2">1000</h1>
+                    <h1 className="static_h1">1000</h1>
                     <div className="static-bottom">
                         <span className="static_desc">Description</span>
                     </div>
                 </div>
                 <div className="static_row">
                     <h2 className="static_h2">Title</h2>
-                    <h1 className="static_h2">1000</h1>
+                    <h1 className="static_h1">1000</h1>
                     <div className="static-bottom">
                         <span className="static_desc">Description</span>
                     </div>
                 </div>
                 <div className="static_row">
                     <h2 className="static_h2">Title</h2>
-                    <h1 className="static_h2">1000</h1>
+                    <h1 className="static_h1">1000</h1>
                     <div className="static-bottom">
                         <span className="static_desc">Description</span>
                     </div>
