aef21c13ae6707a20617eebd723dfd34dc14ceec
AchaUmbaPretre <achandambi@gmail.com>
2024-12-03 09:43:10 +0100

    commit

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -1,13 +1,10 @@
-import { Layout, Menu, message } from 'antd';
+import { Layout, Menu } from 'antd';
 import './sidebar.scss';
 import {
   HomeOutlined,
   ApartmentOutlined,
   LogoutOutlined,
-  TagOutlined,
-  DashboardOutlined,
   BankOutlined,
-  DropboxOutlined,
 } from '@ant-design/icons';
 import { Link } from 'react-router-dom';
 import { useState } from 'react';
@@ -30,63 +27,37 @@ const Sidebar = () => {
   return (
     <div className="sidebar">
       <div className="sidebar-header">
-        LOGINSMART
+        <h1 className="sidebar-h1"><span className='title'>L</span>OGINSMART</h1>
       </div>
-      <Sider style={{ backgroundColor: '#2d3a4b' }}>
+      <Sider width={240} style={{ backgroundColor: 'transparent' }}>
         <Menu
           mode="inline"
-          defaultSelectedKeys={['/']}
+          selectedKeys={[window.location.pathname]}
           openKeys={openKeys}
           onOpenChange={onOpenChange}
           style={{
-            height: '100%',
-            borderRight: 0,
-            width: '100%',
-            
-            backgroundColor: '#2d3a4b', // Fond personnalisé
+            backgroundColor: 'transparent',
+            color: '#ffffff',
+            fontSize: '18px',
           }}
         >
-          <Item key="/" icon={<HomeOutlined style={{ fontSize:'16px'}} />} onClick={toggleMenu}>
+          <Item key="/" icon={<HomeOutlined />} onClick={toggleMenu}>
             <Link to="/">Accueil</Link>
           </Item>
-          <Item key="1" icon={<ApartmentOutlined style={{ fontSize:'16px'}} />} onClick={toggleMenu}>
-            <Link to='/departement'>Département</Link>
+          <Item key="/departement" icon={<ApartmentOutlined />} onClick={toggleMenu}>
+            <Link to="/departement">Carburant</Link>
           </Item>
-          <Item key="2" icon={<DashboardOutlined style={{ fontSize:'16px'}} />} onClick={toggleMenu}>
-            <Link to='/controle'>Contrôle de base</Link>
-          </Item>
-          <SubMenu key="sub1" icon={<BankOutlined style={{ fontSize:'16px'}} />} title="Bâtiment">
-            <Item key="3" onClick={toggleMenu}>
-              <Link to='/batiment'>Liste des Bâtiments</Link>
-            </Item>
-            <Item key="4" onClick={toggleMenu}>
-              <Link to='/liste_bins'>Liste des Bins</Link>
+          <SubMenu key="sub1" icon={<BankOutlined />} title="Véhicule">
+            <Item key="/batiment">
+              <Link to="/batiment">Liste des véhicules</Link>
             </Item>
           </SubMenu>
-          <Item key="1" icon={<ApartmentOutlined style={{ fontSize:'16px'}} />} onClick={toggleMenu}>
-            <Link to='/departement'>Département</Link>
-          </Item>
-          <Item key="2" icon={<DashboardOutlined style={{ fontSize:'16px'}} />} onClick={toggleMenu}>
-            <Link to='/controle'>Contrôle de base</Link>
-          </Item>
-          <SubMenu key="sub1" icon={<BankOutlined style={{ fontSize:'16px'}} />} title="Bâtiment">
-            <Item key="3" onClick={toggleMenu}>
-              <Link to='/batiment'>Liste des Bâtiments</Link>
-            </Item>
-            <Item key="4" onClick={toggleMenu}>
-              <Link to='/liste_bins'>Liste des Bins</Link>
-            </Item>
-          </SubMenu>
-
-          <Item key="logout" icon={<LogoutOutlined style={{ fontSize:'16px'}}/>} className="logout-item">
-            <Link>Déconnecter</Link>
+          <Item key="logout" icon={<LogoutOutlined />} className="logout-item">
+            <Link>Déconnexion</Link>
           </Item>
         </Menu>
       </Sider>
-{/*       <div className="upgrade-section">
-        <div className="upgrade-text">Gain in-depth insights, track performance, and optimize your strategies.</div>
-        <div className="upgrade-button">Upgrade to Pro</div>
-      </div> */}
+
     </div>
   );
 };
