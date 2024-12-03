c4386577f30fd6de638597d958dbd9e3ec0c1e84
AchaUmbaPretre <achandambi@gmail.com>
2024-12-03 10:43:25 +0100

    commit

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -2,7 +2,11 @@ import { Layout, Menu } from 'antd';
 import './sidebar.scss';
 import {
   HomeOutlined,
-  ApartmentOutlined,
+  CarOutlined,
+  TeamOutlined,
+  ToolOutlined,
+  AppstoreAddOutlined,
+  SettingOutlined,
   LogoutOutlined,
   BankOutlined,
 } from '@ant-design/icons';
@@ -37,28 +41,46 @@ const Sidebar = () => {
           onOpenChange={onOpenChange}
           style={{
             backgroundColor: 'transparent',
-            color: '#a4b2c2', // Couleur personnalisée pour le texte
+            color: '#a4b2c2',
             fontSize: '18px',
           }}
         >
-
-          <Item key="/" icon={<HomeOutlined style={{ fontSize: '22px', color: '#a4b2c2' }}/>} onClick={toggleMenu}>
+          <Item key="/" icon={<HomeOutlined style={{ fontSize: '19px', color: '#a4b2c2' }} />} onClick={toggleMenu}>
             <Link to="/">Accueil</Link>
           </Item>
-          <Item key="/departement" icon={<ApartmentOutlined style={{ fontSize: '22px', color: '#a4b2c2' }} />} onClick={toggleMenu}>
+          <Item key="/departement" icon={<AppstoreAddOutlined style={{ fontSize: '19px', color: '#a4b2c2' }} />} onClick={toggleMenu}>
             <Link to="/departement">Carburant</Link>
           </Item>
-          <SubMenu key="sub1" icon={<BankOutlined style={{ fontSize: '22px', color: '#a4b2c2' }} />} title="Véhicule">
-            <Item key="/batiment">
-              <Link to="/batiment">Liste des véhicules</Link>
+          <SubMenu key="sub1" icon={<CarOutlined style={{ fontSize: '19px', color: '#a4b2c2' }} />} title="Véhicule">
+            <Item key="/vehicule">
+              <Link to="/vehicule">Liste des véhicules</Link>
             </Item>
           </SubMenu>
-          <Item key="logout" icon={<LogoutOutlined style={{ fontSize: '22px', color: '#a4b2c2' }} />} className="logout-item">
+          <SubMenu key="sub2" icon={<ToolOutlined style={{ fontSize: '19px', color: '#a4b2c2' }} />} title="Générateur">
+            <Item key="/generateur">
+              <Link to="/generateur">Gestion Générateur</Link>
+            </Item>
+          </SubMenu>
+          <SubMenu key="sub3" icon={<TeamOutlined style={{ fontSize: '19px', color: '#a4b2c2' }} />} title="Personnel">
+            <Item key="/personnel">
+              <Link to="/personnel">Gestion du Personnel</Link>
+            </Item>
+          </SubMenu>
+          <SubMenu key="sub4" icon={<BankOutlined style={{ fontSize: '19px', color: '#a4b2c2' }} />} title="Approvisionnement">
+            <Item key="/approvisionnement">
+              <Link to="/approvisionnement">Suivi Approvisionnement</Link>
+            </Item>
+          </SubMenu>
+          <SubMenu key="sub5" icon={<SettingOutlined style={{ fontSize: '19px', color: '#a4b2c2' }} />} title="Administrateur">
+            <Item key="/administrateur">
+              <Link to="/administrateur">Gestion des Admins</Link>
+            </Item>
+          </SubMenu>
+          <Item key="logout" icon={<LogoutOutlined style={{ fontSize: '20px', color: '#a4b2c2' }} />} className="logout-item">
             <Link>Déconnexion</Link>
           </Item>
         </Menu>
       </Sider>
-
     </div>
   );
 };
