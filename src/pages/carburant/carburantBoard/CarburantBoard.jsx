43a64912c33f721d53f54eee3f95a42630d33065
AchaUmbaPretre <achandambi@gmail.com>
2024-12-09 13:04:24 +0100

    commit

---
+++ src/pages/carburant/carburantBoard/CarburantBoard.jsx
@@ -0,0 +1,15 @@
+import './carburantBoard.scss'
+
+const CarburantBoard = () => {
+  return (
+    <>
+        <div className="carburantBord">
+            <div className="carburantBord-wrapper">
+                
+            </div>
+        </div>
+    </>
+  )
+}
+
+export default CarburantBoard
