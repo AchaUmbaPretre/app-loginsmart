19b1bfe4db8a5208719f73519e020946a1004b4b
AchaUmbaPretre <achandambi@gmail.com>
2024-12-09 11:42:44 +0100

    commit

--- src/pages/carburant/carburantForm/CarburantForm.jsx
+++ src/pages/carburant/carburantForm/CarburantForm.jsx
@@ -1,8 +1,24 @@
 import React from 'react'
+import './carburantForm.scss'
 
 const CarburantForm = () => {
+
   return (
-    <div>CarburantForm</div>
+    <>
+        <div className="carburantForm">
+            <div className="vehicule_row_title">
+                <h2 className="title_h2">Formulaire du Prelevement carburant</h2>
+            </div>
+            <div className="carburantForm-wrapper">
+                <div className="carburantForm-left">
+                    
+                </div>
+                <div className="carburantForm-right">
+                    vvvvvvv
+                </div>
+            </div>
+        </div>
+    </>
   )
 }
 
