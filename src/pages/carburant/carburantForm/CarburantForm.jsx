45618825861d5345dd7d0e88df8e119f96123d22
AchaUmbaPretre <achandambi@gmail.com>
2024-12-09 12:03:32 +0100

    commit

--- src/pages/carburant/carburantForm/CarburantForm.jsx
+++ src/pages/carburant/carburantForm/CarburantForm.jsx
@@ -1,7 +1,15 @@
-import React from 'react'
+import { Button, Col, Form, Input, Row, Skeleton, Space } from 'antd';
 import './carburantForm.scss'
+import { useState } from 'react';
 
 const CarburantForm = () => {
+    const [form] = Form.useForm();
+    const [isLoading, setIsLoading] = useState(false);
+    const [loadingData, setLoadingData] = useState(false);
+
+    const onFinish = () => {
+
+    }
 
   return (
     <>
@@ -11,7 +19,148 @@ const CarburantForm = () => {
             </div>
             <div className="carburantForm-wrapper">
                 <div className="carburantForm-left">
-                    
+                <Form
+                    form={form}
+                    name="validateOnly"
+                    layout="vertical"
+                    autoComplete="off"
+                    className="custom-form"
+                    onFinish={onFinish}
+                >
+                    <Row gutter={12}>
+                        <Col xs={24} md={12}>
+                            <Form.Item
+                                name="immatriculation"
+                                label="Immatriculation"
+                                rules={[
+                                    {
+                                        required: true,
+                                        message: 'Veuillez fournir une immatriculation...',
+                                    },
+                                ]}
+                            >
+                                {loadingData ? <Skeleton.Input active={true} /> : <Input placeholder="Saisir l'immatriculation" />}
+                            </Form.Item>
+                        </Col>
+
+                        <Col xs={24} md={12}>
+                            <Form.Item
+                                name="immatriculation"
+                                label="Immatriculation"
+                                rules={[
+                                    {
+                                        required: true,
+                                        message: 'Veuillez fournir une immatriculation...',
+                                    },
+                                ]}
+                            >
+                                {loadingData ? <Skeleton.Input active={true} /> : <Input placeholder="Saisir l'immatriculation" />}
+                            </Form.Item>
+                        </Col>
+
+                        <Col xs={24} md={12}>
+                            <Form.Item
+                                name="immatriculation"
+                                label="Immatriculation"
+                                rules={[
+                                    {
+                                        required: true,
+                                        message: 'Veuillez fournir une immatriculation...',
+                                    },
+                                ]}
+                            >
+                                {loadingData ? <Skeleton.Input active={true} /> : <Input placeholder="Saisir l'immatriculation" />}
+                            </Form.Item>
+                        </Col>
+
+                        <Col xs={24} md={12}>
+                            <Form.Item
+                                name="immatriculation"
+                                label="Immatriculation"
+                                rules={[
+                                    {
+                                        required: true,
+                                        message: 'Veuillez fournir une immatriculation...',
+                                    },
+                                ]}
+                            >
+                                {loadingData ? <Skeleton.Input active={true} /> : <Input placeholder="Saisir l'immatriculation" />}
+                            </Form.Item>
+                        </Col>
+
+                        <Col xs={24} md={12}>
+                            <Form.Item
+                                name="immatriculation"
+                                label="Immatriculation"
+                                rules={[
+                                    {
+                                        required: true,
+                                        message: 'Veuillez fournir une immatriculation...',
+                                    },
+                                ]}
+                            >
+                                {loadingData ? <Skeleton.Input active={true} /> : <Input placeholder="Saisir l'immatriculation" />}
+                            </Form.Item>
+                        </Col>
+
+                        <Col xs={24} md={12}>
+                            <Form.Item
+                                name="immatriculation"
+                                label="Immatriculation"
+                                rules={[
+                                    {
+                                        required: true,
+                                        message: 'Veuillez fournir une immatriculation...',
+                                    },
+                                ]}
+                            >
+                                {loadingData ? <Skeleton.Input active={true} /> : <Input placeholder="Saisir l'immatriculation" />}
+                            </Form.Item>
+                        </Col>
+
+                        <Col xs={24} md={12}>
+                            <Form.Item
+                                name="immatriculation"
+                                label="Immatriculation"
+                                rules={[
+                                    {
+                                        required: true,
+                                        message: 'Veuillez fournir une immatriculation...',
+                                    },
+                                ]}
+                            >
+                                {loadingData ? <Skeleton.Input active={true} /> : <Input placeholder="Saisir l'immatriculation" />}
+                            </Form.Item>
+                        </Col>
+
+                        <Col xs={24} md={12}>
+                            <Form.Item
+                                name="immatriculation"
+                                label="Immatriculation"
+                                rules={[
+                                    {
+                                        required: true,
+                                        message: 'Veuillez fournir une immatriculation...',
+                                    },
+                                ]}
+                            >
+                                {loadingData ? <Skeleton.Input active={true} /> : <Input placeholder="Saisir l'immatriculation" />}
+                            </Form.Item>
+                        </Col>
+                        <Col xs={24}>
+                            <Form.Item>
+                                <Space className="button-group">
+                                    <Button type="primary" htmlType="submit" loading={isLoading} disabled={isLoading}>
+                                        {'Ajouter'}
+                                    </Button>
+                                    <Button htmlType="reset">
+                                        Réinitialiser
+                                    </Button>
+                                </Space>
+                            </Form.Item>
+                        </Col>
+                    </Row>
+                </Form>
                 </div>
                 <div className="carburantForm-right">
                     vvvvvvv
