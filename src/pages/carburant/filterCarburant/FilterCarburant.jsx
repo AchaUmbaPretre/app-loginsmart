f47adbd413fbc071256afe3899f9c5e8c2990b3f
AchaUmbaPretre <achandambi@gmail.com>
2024-12-09 15:39:22 +0100

    commit

--- src/pages/carburant/filterCarburant/FilterCarburant.jsx
+++ src/pages/carburant/filterCarburant/FilterCarburant.jsx
@@ -1,31 +1,44 @@
-import './filterCarburant.scss'
-import {  Select } from 'antd';
+import './filterCarburant.scss';
+import { Select, DatePicker } from 'antd';
 const { Option } = Select;
-
+const { RangePicker } = DatePicker;
 
 const FilterCarburant = () => {
   return (
-    <>
-        <div className="filterCarburant">
-            <div className="filterCarburant-wrapper">
-                <div className="form_control">
-                    <label htmlFor="" className="label-carburant">Immatriculation</label>
-                    <Select  mode="multiple" showSearch placeholder="Choisir un lubrifiant moteur">
-                        <Option value="1">Lubrifiant 1</Option>
-                        <Option value="2">Lubrifiant 2</Option>
-                    </Select>
-                </div>
-                <div className="form_control">
-
-                </div>
-                <div className="form_control">
-
-                </div>
-            
-            </div>
+    <div className="filterCarburant">
+      <div className="filterCarburant-wrapper">
+        {/* Immatriculation Filter */}
+        <div className="form_control">
+          <label htmlFor="immatriculation" className="label-carburant">
+            Immatriculation
+          </label>
+          <Select
+            id="immatriculation"
+            mode="multiple"
+            showSearch
+            placeholder="Choisir un lubrifiant moteur"
+            style={{ width: '100%' }}
+          >
+            <Option value="1">Lubrifiant 1</Option>
+            <Option value="2">Lubrifiant 2</Option>
+          </Select>
         </div>
-    </>
-  )
-}
 
-export default FilterCarburant
+        {/* Date Filter */}
+        <div className="form_control">
+          <label htmlFor="date-range" className="label-carburant">
+            Période
+          </label>
+          <RangePicker
+            id="date-range"
+            format="YYYY-MM-DD"
+            className="date-picker"
+            placeholder={['Date début', 'Date fin']}
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default FilterCarburant;
