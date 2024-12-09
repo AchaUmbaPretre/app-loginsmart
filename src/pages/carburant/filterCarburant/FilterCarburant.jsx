247ed8feabced1a6d286bb0c89ed7fe62616cbbb
AchaUmbaPretre <achandambi@gmail.com>
2024-12-09 15:28:12 +0100

    commit

--- src/pages/carburant/filterCarburant/FilterCarburant.jsx
+++ src/pages/carburant/filterCarburant/FilterCarburant.jsx
@@ -1,8 +1,30 @@
-import React from 'react'
+import './filterCarburant.scss'
+import {  Select } from 'antd';
+const { Option } = Select;
+
 
 const FilterCarburant = () => {
   return (
-    <div>FilterCarburant</div>
+    <>
+        <div className="filterCarburant">
+            <div className="filterCarburant-wrapper">
+                <div className="form_control">
+                    <label htmlFor="" className="label-carburant">Immatriculation</label>
+                    <Select  mode="multiple" showSearch placeholder="Choisir un lubrifiant moteur">
+                        <Option value="1">Lubrifiant 1</Option>
+                        <Option value="2">Lubrifiant 2</Option>
+                    </Select>
+                </div>
+                <div className="form_control">
+
+                </div>
+                <div className="form_control">
+
+                </div>
+            
+            </div>
+        </div>
+    </>
   )
 }
 
