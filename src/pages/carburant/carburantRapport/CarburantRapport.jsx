5d4219b3bba083b577d33f8db28e5e662ebb2432
AchaUmbaPretre <achandambi@gmail.com>
2024-12-10 15:33:43 +0100

    commit

--- src/pages/carburant/carburantRapport/CarburantRapport.jsx
+++ src/pages/carburant/carburantRapport/CarburantRapport.jsx
@@ -1,12 +1,17 @@
 import './carburantRapport.scss'
 
 const CarburantRapport = () => {
-    
+
   return (
     <>
         <div className="carburantRapport">
             <div className="carburantRapport_wrapper">
-                Carburant rapport
+                <div className="carburantRapport_row_title">
+                    <h2 className="carburant_h2">Rapport consomation carburant par vehicule (sur les 360 dernier jours )</h2>
+                </div>
+                <div className="carburantRapport_rows">
+                    
+                </div>
             </div>
         </div>
     </>
