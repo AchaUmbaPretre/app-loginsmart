2ed379455f587610806efff0dda1b4e122452bb9
AchaUmbaPretre <achandambi@gmail.com>
2024-12-10 14:22:53 +0100

    commit

--- src/pages/vehicule/Vehicule.jsx
+++ src/pages/vehicule/Vehicule.jsx
@@ -28,64 +28,43 @@ const Vehicule = () => {
             width: "3%" 
           },
         {
-          title: 'Name',
-          dataIndex: 'name',
+          title: 'Immatriculation',
+          dataIndex: 'immatriculation',
         },
         {
-          title: 'Chinese Score',
-          dataIndex: 'chinese',
+          title: 'Marque',
+          dataIndex: 'marque',
           sorter: {
             compare: (a, b) => a.chinese - b.chinese,
             multiple: 3,
           },
         },
         {
-          title: 'Math Score',
-          dataIndex: 'math',
-          sorter: {
-            compare: (a, b) => a.math - b.math,
-            multiple: 2,
-          },
+          title: 'Modèle',
+          dataIndex: 'modele'
         },
         {
-          title: 'English Score',
-          dataIndex: 'english',
+          title: 'Année',
+          dataIndex: 'annee',
           sorter: {
-            compare: (a, b) => a.english - b.english,
+            compare: (a, b) => a.annee - b.annee,
             multiple: 1,
           },
         },
-      ];
-      const data = [
         {
-          key: '1',
-          name: 'John Brown',
-          chinese: 98,
-          math: 60,
-          english: 70,
+          title: 'Categorie',
+          dataIndex: 'categorie'
         },
         {
-          key: '2',
-          name: 'Jim Green',
-          chinese: 98,
-          math: 66,
-          english: 89,
+          title: 'Affectation',
+          dataIndex: 'affectation'
         },
         {
-          key: '3',
-          name: 'Joe Black',
-          chinese: 98,
-          math: 90,
-          english: 70,
-        },
-        {
-          key: '4',
-          name: 'Jim Red',
-          chinese: 88,
-          math: 99,
-          english: 89,
+          title: 'Actions',
+          dataIndex: 'actions'
         },
       ];
+
       const onChange = (pagination, filters, sorter, extra) => {
         console.log('params', pagination, filters, sorter, extra);
       };
