c151f394dbf025e9f7de9ca7735820bde967f802
AchaUmbaPretre <achandambi@gmail.com>
2024-12-10 09:41:35 +0100

    commit

--- src/pages/carburant/consomCarburant/ConsomCarburant.jsx
+++ src/pages/carburant/consomCarburant/ConsomCarburant.jsx
@@ -8,6 +8,9 @@ const ConsomCarburant = () => {
             <div className="consomCarburant-row-title">
               <h2 className="title_h2">CALCUL CONSOMATION</h2>
             </div>
+            <div className="consomCarburant_rows">
+              
+            </div>
           </div>
         </div>
     </>
