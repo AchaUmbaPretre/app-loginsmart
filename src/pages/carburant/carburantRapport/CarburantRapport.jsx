8fb3304b27496134684662ed85641ef9a4a47b27
AchaUmbaPretre <achandambi@gmail.com>
2024-12-10 14:54:54 +0100

    commit

---
+++ src/pages/carburant/carburantRapport/CarburantRapport.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const CarburantRapport = () => {
+  return (
+    <>
+        <div className="carburantRapport">
+            <div className="carburantRapport_wrapper">
+                
+            </div>
+        </div>
+    </>
+  )
+}
+
+export default CarburantRapport
