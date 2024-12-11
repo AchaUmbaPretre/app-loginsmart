006482c0821805ef03be0e644af541951ff67db7
AchaUmbaPretre <achandambi@gmail.com>
2024-12-11 10:31:03 +0100

    commit

--- src/pages/carburant/carburantRapport/carburantTabInfo/CarburantTabInfo.jsx
+++ src/pages/carburant/carburantRapport/carburantTabInfo/CarburantTabInfo.jsx
@@ -1,12 +1,21 @@
 import './carburantTabInfo.scss'
 
 const CarburantTabInfo = () => {
-    
+
   return (
     <>
         <div className="carburantTabInfo">
             <div className="carburantTabInfo_wrapper">
-                ffffffffff
+                <div className="carburantTabInfo-left">
+                    <h2 className="carburant_h2">Information générales</h2>
+
+                </div>
+                <div className="carburantTabInfo-center">
+
+                </div>
+                <div className="carburantTabInfo-bottom">
+
+                </div>
             </div>
         </div>
     </>
