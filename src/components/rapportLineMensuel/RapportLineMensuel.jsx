03810bda44be673c61095187cf78addd15637689
AchaUmbaPretre <achandambi@gmail.com>
2024-12-12 12:43:13 +0100

    commit

--- src/components/rapportLineMensuel/RapportLineMensuel.jsx
+++ src/components/rapportLineMensuel/RapportLineMensuel.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Bar } from 'react-chartjs-2';
 import { Chart as ChartJS, CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend } from 'chart.js';
-
+import './rapportLineMensuel.scss'
 // Enregistrer les composants nécessaires pour Chart.js
 ChartJS.register(CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend);
 
--- src/components/rapportPieMensuel/RapportPieMensuel.jsx
+++ src/components/rapportPieMensuel/RapportPieMensuel.jsx
@@ -1,13 +1,55 @@
-import React from 'react'
+import React from 'react';
+import { Doughnut } from 'react-chartjs-2';
+import { Chart as ChartJS, ArcElement, Tooltip, Legend } from 'chart.js';
+import './rapportPieMensuel.scss'
+ChartJS.register(ArcElement, Tooltip, Legend);
 
 const RapportPieMensuel = () => {
-  return (
-    <>
-        <div className="rapportPieMensuel">
-        RapportPieMensuel
-        </div>
-    </>
-  )
-}
+  const data = {
+    labels: ['Diesel', 'Essence'],
+    datasets: [
+      {
+        label: 'Consommation',
+        data: [60, 40],
+        backgroundColor: ['#1a73e8', '#fbbc04'],
+        hoverOffset: 4,
+      },
+    ],
+  };
 
-export default RapportPieMensuel
+  // Options pour le Doughnut Chart
+  const options = {
+    responsive: true,
+    plugins: {
+      legend: {
+        position: 'top', // Position de la légende
+      },
+      title: {
+        display: true,
+        text: 'Répartition de la Consommation Mensuelle', // Titre du diagramme
+      },
+      tooltip: {
+        callbacks: {
+          label: function (context) {
+            const value = context.raw;
+            const total = context.dataset.data.reduce((a, b) => a + b, 0);
+            const percentage = ((value / total) * 100).toFixed(2) + '%';
+            return `${context.label}: ${percentage}`;
+          },
+        },
+      },
+    },
+  };
+
+  return (
+    <div className="rapportPieMensuel">
+      <div className="chart-container">
+        <div className="chart">
+          <Doughnut data={data} options={options} />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default RapportPieMensuel;
