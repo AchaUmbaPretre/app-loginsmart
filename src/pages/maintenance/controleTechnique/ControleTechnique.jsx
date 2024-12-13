436529696db5aae27486367afcffa348edbec654
AchaUmbaPretre <achandambi@gmail.com>
2024-12-13 14:01:46 +0100

    commit

--- src/pages/maintenance/controleTechnique/ControleTechnique.jsx
+++ src/pages/maintenance/controleTechnique/ControleTechnique.jsx
@@ -1,9 +1,141 @@
-import React from 'react'
+import { Breadcrumb, Button, Input, Modal, Space, Table } from 'antd';
+import { PlusCircleOutlined, SearchOutlined, FilterOutlined } from '@ant-design/icons';
+import { useState } from 'react';
+import Maintenance_form from './maintenance_form/Maintenance_form';
 
 const ControleTechnique = () => {
-  return (
-    <div>ControleTechnique</div>
-  )
-}
+    const [filterVisible, setFilterVisible] = useState(false);
+    const [modalType, setModalType] = useState(null);
+    const [idVehicule, setIdVehicule] = useState('');
 
-export default ControleTechnique
+    const closeAllModals = () => {
+      setModalType(null);
+    };
+  
+    const openModal = (type, idVehicule = '') => {
+      closeAllModals();
+      setModalType(type);
+    };
+  
+    const handleAdd = (idVehicule) =>{
+      openModal('add', idVehicule )
+    }
+
+    const handFilter = () => {
+        setFilterVisible(!filterVisible)
+      }
+
+    const handleFilterChange = () => {
+
+    }
+
+    const columns = [
+        { 
+            title: '#', 
+            dataIndex: 'id', 
+            key: 'id', 
+            render: (text, record, index) => index + 1, 
+            width: "3%" 
+          },
+        {
+          title: 'Immatriculation',
+          dataIndex: 'immatricule',
+        },
+        {
+          title: 'Marque',
+          dataIndex: 'marque'
+        },
+        {
+          title: 'Date controle',
+          dataIndex: 'date_entree'
+        },
+        {
+          title: 'Date validité',
+          dataIndex: 'date_sortie'
+        },
+        {
+            title: 'Description',
+            dataIndex: 'description'
+        },
+        {
+            title: 'Categorie',
+            dataIndex: 'categorie'
+        },
+        {
+            title: 'Chauffeur',
+            dataIndex: 'fournisseur'
+        },
+        {
+            title: 'Utilisateur',
+            dataIndex: 'utilisateur'
+        },
+        {
+            title: 'Actions',
+            dataIndex: 'actions'
+        }
+      ];
+      const data = [];
+      const onChange = (pagination, filters, sorter, extra) => {
+        console.log('params', pagination, filters, sorter, extra);
+      };
+
+  return (
+    <div className="chauffeur">
+        <div className="chauffeur_top">
+            <div className="chauffeur_top_left">
+                <h2 className="chauffeur_h2">LISTE DES CONTROLES TECHNIQUES</h2>
+                <Breadcrumb
+                    separator=">"
+                    items={[
+                        { title: 'Accueil', href: '/' },
+                        { title: 'Consommation', href: '/consommation_carburant' },
+                        { title: 'Recherche globale', href: '/gestion' },
+                        { title: 'Carburant' },
+                    ]}
+                    className="chauffeur_breadcrumb"
+                />
+            </div>
+            <div className="chauffeur_top_right">
+                <Space size="middle">
+                <Input
+                    placeholder="Rechercher..."
+                    prefix={<SearchOutlined />}
+                    className="chauffeur_search"
+                />
+                <Button icon={<FilterOutlined />} onClick={handFilter} className="chauffeur_filter">
+                    Filtres
+                </Button>
+                <Button
+                    className="chauffeur_btn"
+                    type="primary"
+                    icon={<PlusCircleOutlined />}
+                    onClick={handleAdd}
+                >
+                    Nouveau carburant
+                </Button>
+                </Space>
+            </div>
+        </div>
+{/*         {filterVisible && <FilterCarburant onFilter={handleFilterChange}/>}
+ */}        <div className="chauffeur_bottom">
+            <Table 
+                columns={columns} 
+                dataSource={data} 
+                onChange={onChange} 
+            />
+        </div>
+        <Modal
+          title=""
+          visible={modalType === 'add'}
+          onCancel={closeAllModals}
+          footer={null}
+          width={1025}
+          centered
+        >
+            <Maintenance_form/>
+        </Modal>
+    </div>
+  );
+};
+
+export default ControleTechnique;
