a4d3f10aa518e8dc5e210cf6d9813a76b1478056
AchaUmbaPretre <achandambi@gmail.com>
2024-12-13 16:20:48 +0100

    commit

---
+++ src/pages/generateur/generateurForm/GenerateurForm.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const GenerateurForm = () => {
+  return (
+    <>
+        <div className="generateurForm">
+            <div className="vehicule_row_title">
+                <h2 className="title_h2">Enregistrer un nouveau vehicule</h2>
+            </div>
+            <div className="generateurForm_wrapper">
+                
+            </div>
+        </div>
+    </>
+  )
+}
+
+export default GenerateurForm
