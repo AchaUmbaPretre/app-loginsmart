9f76e175a78f358af463444a539d99bad1e0a874
AchaUmbaPretre <achandambi@gmail.com>
2024-12-13 16:46:16 +0100

    commit

--- src/pages/personnel/Personnel.jsx
+++ src/pages/personnel/Personnel.jsx
@@ -12,64 +12,24 @@ const Personnel = ({ onAddChauffeur }) => {
             width: "3%" 
           },
         {
-          title: 'Name',
-          dataIndex: 'name',
+          title: 'Nom',
+          dataIndex: 'nom',
         },
         {
-          title: 'Chinese Score',
-          dataIndex: 'chinese',
-          sorter: {
-            compare: (a, b) => a.chinese - b.chinese,
-            multiple: 3,
-          },
+          title: 'Prénom',
+          dataIndex: 'prenom'
         },
         {
-          title: 'Math Score',
-          dataIndex: 'math',
-          sorter: {
-            compare: (a, b) => a.math - b.math,
-            multiple: 2,
-          },
+          title: 'Email',
+          dataIndex: 'email'
         },
         {
-          title: 'English Score',
-          dataIndex: 'english',
-          sorter: {
-            compare: (a, b) => a.english - b.english,
-            multiple: 1,
-          },
+          title: 'Role',
+          dataIndex: 'role'
         },
       ];
-      const data = [
-        {
-          key: '1',
-          name: 'John Brown',
-          chinese: 98,
-          math: 60,
-          english: 70,
-        },
-        {
-          key: '2',
-          name: 'Jim Green',
-          chinese: 98,
-          math: 66,
-          english: 89,
-        },
-        {
-          key: '3',
-          name: 'Joe Black',
-          chinese: 98,
-          math: 90,
-          english: 70,
-        },
-        {
-          key: '4',
-          name: 'Jim Red',
-          chinese: 88,
-          math: 99,
-          english: 89,
-        },
-      ];
+      const data = [];
+      
       const onChange = (pagination, filters, sorter, extra) => {
         console.log('params', pagination, filters, sorter, extra);
       };
