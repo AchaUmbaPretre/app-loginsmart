d220cb34be5a3560a04235136d9fa1db3ee3a357
AchaUmbaPretre <achandambi@gmail.com>
2024-12-13 16:14:52 +0100

    commit

--- src/pages/generateur/Generateur.jsx
+++ src/pages/generateur/Generateur.jsx
@@ -12,64 +12,44 @@ const Generateur = ({ onAddChauffeur }) => {
             width: "3%" 
         },
         {
-          title: 'Name',
-          dataIndex: 'name',
+          title: 'Code',
+          dataIndex: 'code',
         },
         {
-          title: 'Chinese Score',
-          dataIndex: 'chinese',
-          sorter: {
-            compare: (a, b) => a.chinese - b.chinese,
-            multiple: 3,
-          },
+          title: 'Marque',
+          dataIndex: 'marque'
         },
         {
-          title: 'Math Score',
-          dataIndex: 'math',
-          sorter: {
-            compare: (a, b) => a.math - b.math,
-            multiple: 2,
-          },
+          title: 'Carburant',
+          dataIndex: 'carburant'
         },
         {
-          title: 'English Score',
-          dataIndex: 'english',
-          sorter: {
-            compare: (a, b) => a.english - b.english,
-            multiple: 1,
-          },
+          title: 'Puissance',
+          dataIndex: 'puissance'
         },
+        {
+          title: 'Dimension',
+          dataIndex: 'dimension'
+        },
+        {
+          title: 'Mise en service',
+          dataIndex: 'mise_en_service'
+        },
+        {
+          title: 'Reservoir',
+          dataIndex: 'reservoir'
+        },
+        {
+          title: 'Affectation',
+          dataIndex: 'affectation'
+        },
+        {
+          title: 'Actions',
+          dataIndex: 'actions'
+        }
       ];
-      const data = [
-        {
-          key: '1',
-          name: 'John Brown',
-          chinese: 98,
-          math: 60,
-          english: 70,
-        },
-        {
-          key: '2',
-          name: 'Jim Green',
-          chinese: 98,
-          math: 66,
-          english: 89,
-        },
-        {
-          key: '3',
-          name: 'Joe Black',
-          chinese: 98,
-          math: 90,
-          english: 70,
-        },
-        {
-          key: '4',
-          name: 'Jim Red',
-          chinese: 88,
-          math: 99,
-          english: 89,
-        },
-      ];
+      const data = [];
+      
       const onChange = (pagination, filters, sorter, extra) => {
         console.log('params', pagination, filters, sorter, extra);
       };
