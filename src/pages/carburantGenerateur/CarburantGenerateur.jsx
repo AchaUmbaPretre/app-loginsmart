97c3ee97f224bbe4c01c9b04b748d0ae9be3796e
AchaUmbaPretre <achandambi@gmail.com>
2024-12-17 16:54:17 +0100

    commit

--- src/pages/carburantGenerateur/CarburantGenerateur.jsx
+++ src/pages/carburantGenerateur/CarburantGenerateur.jsx
@@ -5,6 +5,7 @@ import CarburantGenerateurForm from './carburantGenerateurForm/CarburantGenerate
 
 const CarburantGenerateur = () => {
   const [modalType, setModalType] = useState(null);
+  const [data, setData] = useState([]);
 
   const closeAllModals = () => {
     setModalType(null);
@@ -93,36 +94,6 @@ const CarburantGenerateur = () => {
         }
       ];
 
-      const data = [
-        {
-          key: '1',
-          name: 'John Brown',
-          chinese: 98,
-          math: 60,
-          english: 70,
-        },
-        {
-          key: '2',
-          name: 'Jim Green',
-          chinese: 98,
-          math: 66,
-          english: 89,
-        },
-        {
-          key: '3',
-          name: 'Joe Black',
-          chinese: 98,
-          math: 90,
-          english: 70,
-        },
-        {
-          key: '4',
-          name: 'Jim Red',
-          chinese: 88,
-          math: 99,
-          english: 89,
-        },
-      ];
       const onChange = (pagination, filters, sorter, extra) => {
         console.log('params', pagination, filters, sorter, extra);
       };
