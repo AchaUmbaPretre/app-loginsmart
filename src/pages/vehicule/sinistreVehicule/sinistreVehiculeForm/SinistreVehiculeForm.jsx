edce139437341b8e4225b8e444bd7256ddeab554
AchaUmbaPretre <achandambi@gmail.com>
2024-12-17 11:36:44 +0100

    commit

--- src/pages/vehicule/sinistreVehicule/sinistreVehiculeForm/SinistreVehiculeForm.jsx
+++ src/pages/vehicule/sinistreVehicule/sinistreVehiculeForm/SinistreVehiculeForm.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import { Form, Input, Row, Col, Select, DatePicker, Skeleton, Divider, InputNumber, Radio, Space } from 'antd';
+import { Form, Input, Row, Col, Select, DatePicker, Skeleton, Divider, InputNumber, Radio, Space, Checkbox } from 'antd';
 const { Option } = Select;
 
 const SinistreVehiculeForm = () => {
@@ -128,7 +128,7 @@ const SinistreVehiculeForm = () => {
                             </Form.Item>
                         </Col>
 
-                        <Col xs={24} md={12}>
+                        <Col xs={24} md={16}>
                             <Form.Item
                                 name="id_chauffeur"
                                 label="Chauffeur"
@@ -146,7 +146,7 @@ const SinistreVehiculeForm = () => {
                             </Form.Item>
                         </Col>
 
-                        <Col xs={24} md={12}>
+                        <Col xs={24} md={8}>
                             <Form.Item
                                 name="partie_tierce"
                                 label="Partie tierce"
@@ -163,9 +163,129 @@ const SinistreVehiculeForm = () => {
                             </Form.Item>
                         </Col>
 
-                        <Divider className='title_row'>Autre information</Divider>
-                        
+                        <Divider className='title_row'>Autres informations</Divider>
 
+                        <div style={{display:'flex', alignItems:'center', justifyContent:'space-between', width:'100%'}}>
+                            <div>
+                                <h4>Dommage aux tiers</h4>
+                                <div>
+                                    <Col span={8}>
+                                        <Checkbox value="vehicule">Vehicules</Checkbox>
+                                    </Col>
+                                    <Col span={8}>
+                                        <Checkbox value="objet">Objet</Checkbox>
+                                    </Col>
+                                </div>
+                            </div>
+
+                            <div>
+                                <h4>Largeur</h4>
+                                <div>
+                                <Checkbox.Group
+                                        style={{
+                                        width: '100%',
+                                        }}
+                                    >
+                                        <Row>
+                                            <Col span={12}>
+                                                <Checkbox value="A">Passager(s) abord</Checkbox>
+                                            </Col>
+                                            <Col span={12}>
+                                                <Checkbox value="B">Passager(s) à bord(tiers)</Checkbox>
+                                            </Col>
+                                            <Col span={12}>
+                                                <Checkbox value="C">Blessé(s)</Checkbox>
+                                            </Col>
+                                            <Col span={12}>
+                                                <Checkbox value="D">Témoin(s)</Checkbox>
+                                            </Col>
+                                        </Row>
+                                    </Checkbox.Group>
+                                </div>
+                            </div>
+                        </div>
+
+                        <Divider className='title_row'></Divider>
+                        
+                        <div style={{display:'flex', alignItems:'center', justifyContent:'space-between', width:'100%'}}>
+                            <div>
+                                <h4>Y a-t-il eu Débit de fuite ?</h4>
+                                <Col xs={24} md={8}>
+                                    <Radio.Group>
+                                        <Radio value={1}>Oui</Radio>
+                                        <Radio value={2}>Non</Radio>
+                                    </Radio.Group>
+                                </Col>
+                            </div>
+
+                            <div>
+                                <h4>Y a-t-il eu Débit de fuite ?</h4>
+                                <Col xs={24} md={8}>
+                                    <Radio.Group>
+                                        <Radio value={1}>Oui</Radio>
+                                        <Radio value={2}>Non</Radio>
+                                    </Radio.Group>
+                                </Col>
+                            </div>
+                        </div>
+                        <Col xs={24} md={12}>
+                            <Form.Item
+                                name="num_verbal"
+                                label="N°Proces verbale"
+                                rules={[
+                                    {
+                                        required: true,
+                                        message: 'Veuillez fournir un lieu...',
+                                    },
+                                ]}
+                            >
+                                {loadingData ? <Skeleton.Input active={true} /> : <Input placeholder="Saisir le numero de proces verbale" />}
+                            </Form.Item>
+                        </Col>
+
+                        <Col xs={24} md={12}>
+                            <Form.Item
+                                name="date_etablissement"
+                                label="Date Etablissement"
+                                rules={[
+                                    {
+                                        required: true,
+                                        message: 'Veuillez fournir une date...',
+                                    },
+                                ]}
+                            >
+                                {loadingData ? <Skeleton.Input active={true} /> : <DatePicker style={{width:'100%'}} />}
+                            </Form.Item>
+                        </Col>
+
+                        <Col xs={24} md={12}>
+                            <Form.Item
+                                name="num_verbal"
+                                label="Nom agent qualité"
+                                rules={[
+                                    {
+                                        required: true,
+                                        message: 'Veuillez fournir un lieu...',
+                                    },
+                                ]}
+                            >
+                                {loadingData ? <Skeleton.Input active={true} /> : <Input placeholder="Saisir le nom d un agent" />}
+                            </Form.Item>
+                        </Col>
+                        <Col xs={24} md={12}>
+                            <Form.Item
+                                name="Gandarmerie"
+                                label="Gandarmerie"
+                                rules={[
+                                    {
+                                        required: true,
+                                        message: 'Veuillez fournir une Gandarmerie...',
+                                    },
+                                ]}
+                            >
+                                {loadingData ? <Skeleton.Input active={true} /> : <Input placeholder="Saisir...." />}
+                            </Form.Item>
+                        </Col>
                     </Row>
                 </Form>
             </div>
