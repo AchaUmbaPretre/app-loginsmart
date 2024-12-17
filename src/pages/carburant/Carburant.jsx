72fa8a40972dafd8fa8dfd9d23252bbc4079a80b
AchaUmbaPretre <achandambi@gmail.com>
2024-12-17 09:56:46 +0100

    commit

--- src/pages/carburant/Carburant.jsx
+++ src/pages/carburant/Carburant.jsx
@@ -9,6 +9,7 @@ const Carburant = () => {
     const [filterVisible, setFilterVisible] = useState(false);
     const [modalType, setModalType] = useState(null);
     const [idVehicule, setIdVehicule] = useState('');
+    const [data, setData] = useState([]);
 
     const closeAllModals = () => {
       setModalType(null);
@@ -92,36 +93,7 @@ const Carburant = () => {
             },
         }
       ];
-      const data = [
-        {
-          key: '1',
-          name: 'John Brown',
-          chinese: 98,
-          math: 60,
-          english: 70,
-        },
-        {
-          key: '2',
-          name: 'Jim Green',
-          chinese: 98,
-          math: 66,
-          english: 89,
-        },
-        {
-          key: '3',
-          name: 'Joe Black',
-          chinese: 98,
-          math: 90,
-          english: 70,
-        },
-        {
-          key: '4',
-          name: 'Jim Red',
-          chinese: 88,
-          math: 99,
-          english: 89,
-        },
-      ];
+
       const onChange = (pagination, filters, sorter, extra) => {
         console.log('params', pagination, filters, sorter, extra);
       };
