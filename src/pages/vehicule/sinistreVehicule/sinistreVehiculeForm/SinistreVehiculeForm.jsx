4284442f44ac9140d954ce7fd1cf6398be88ca00
AchaUmbaPretre <achandambi@gmail.com>
2024-12-17 10:16:29 +0100

    commit

--- src/pages/vehicule/sinistreVehicule/sinistreVehiculeForm/SinistreVehiculeForm.jsx
+++ src/pages/vehicule/sinistreVehicule/sinistreVehiculeForm/SinistreVehiculeForm.jsx
@@ -1,8 +1,33 @@
 import React from 'react'
+import { Button, Form, Upload, Input, Row, Col, Select, DatePicker, Skeleton, Divider, InputNumber, Radio, Space } from 'antd';
+
 
 const SinistreVehiculeForm = () => {
+    const [form] = Form.useForm();
+
+    const onFinish = () => {
+
+    }
+
   return (
-    <div>SinistreVehiculeForm</div>
+    <>
+        <div className="vehiculeForm">
+            <div className="vehicule_row_title">
+                <h2 className="title_h2">ENREGISTRER UN SINISTRE</h2>
+            </div>
+            <div className="vehiculeForm_wrapper">
+                <Form
+                    form={form}
+                    name="validateOnly"
+                    layout="vertical"
+                    autoComplete="off"
+                    className="custom-form"
+                    onFinish={onFinish}
+                >
+                </Form>
+            </div>
+        </div>
+    </>
   )
 }
 
