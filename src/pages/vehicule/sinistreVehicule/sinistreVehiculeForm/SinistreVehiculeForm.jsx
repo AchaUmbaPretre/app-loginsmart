768a1d814ad7c9855817dc4caee383f058cd1b8e
AchaUmbaPretre <achandambi@gmail.com>
2024-12-17 10:26:28 +0100

    commit

--- src/pages/vehicule/sinistreVehicule/sinistreVehiculeForm/SinistreVehiculeForm.jsx
+++ src/pages/vehicule/sinistreVehicule/sinistreVehiculeForm/SinistreVehiculeForm.jsx
@@ -1,9 +1,11 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Button, Form, Upload, Input, Row, Col, Select, DatePicker, Skeleton, Divider, InputNumber, Radio, Space } from 'antd';
 
 
 const SinistreVehiculeForm = () => {
     const [form] = Form.useForm();
+    const [loadingData, setLoadingData] = useState(false);
+
 
     const onFinish = () => {
 
@@ -24,6 +26,35 @@ const SinistreVehiculeForm = () => {
                     className="custom-form"
                     onFinish={onFinish}
                 >
+                    <Col xs={24} md={8}>
+                        <Form.Item
+                            name="lieu"
+                            label="Lieu"
+                            rules={[
+                                {
+                                    required: true,
+                                    message: 'Veuillez fournir une immatriculation...',
+                                },
+                            ]}
+                        >
+                                {loadingData ? <Skeleton.Input active={true} /> : <Input placeholder="Saisir l'immatriculation" />}
+                        </Form.Item>
+                    </Col>
+
+                    <Col xs={24} md={8}>
+                        <Form.Item
+                            name="numero_ordre"
+                            label="Numéro d'ordre"
+                            rules={[
+                                {
+                                    required: true,
+                                    message: 'Veuillez fournir un Numéro d ordre... ',
+                                },
+                            ]}
+                        >
+                                {loadingData ? <Skeleton.Input active={true} /> : <Input placeholder="Numéro d'ordre (optionnel)" />}
+                        </Form.Item>
+                    </Col>
                 </Form>
             </div>
         </div>
