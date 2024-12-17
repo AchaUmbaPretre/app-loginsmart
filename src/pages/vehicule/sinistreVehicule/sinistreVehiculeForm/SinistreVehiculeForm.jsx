f57f0fde8e9f06ee479cdb5a0db4e27f2c5ddc14
AchaUmbaPretre <achandambi@gmail.com>
2024-12-17 10:42:44 +0100

    commit

--- src/pages/vehicule/sinistreVehicule/sinistreVehiculeForm/SinistreVehiculeForm.jsx
+++ src/pages/vehicule/sinistreVehicule/sinistreVehiculeForm/SinistreVehiculeForm.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react'
-import { Button, Form, Upload, Input, Row, Col, Select, DatePicker, Skeleton, Divider, InputNumber, Radio, Space } from 'antd';
-
+import { Form, Input, Row, Col, Select, DatePicker, Skeleton, Divider, InputNumber, Radio, Space } from 'antd';
+const { Option } = Select;
 
 const SinistreVehiculeForm = () => {
     const [form] = Form.useForm();
@@ -26,35 +26,70 @@ const SinistreVehiculeForm = () => {
                     className="custom-form"
                     onFinish={onFinish}
                 >
-                    <Col xs={24} md={8}>
-                        <Form.Item
-                            name="lieu"
-                            label="Lieu"
-                            rules={[
-                                {
-                                    required: true,
-                                    message: 'Veuillez fournir une immatriculation...',
-                                },
-                            ]}
-                        >
-                                {loadingData ? <Skeleton.Input active={true} /> : <Input placeholder="Saisir l'immatriculation" />}
-                        </Form.Item>
-                    </Col>
+                    <Row gutter={12}>
+                        <Col xs={24} md={8}>
+                            <Form.Item
+                                name="lieu"
+                                label="Lieu"
+                                rules={[
+                                    {
+                                        required: true,
+                                        message: 'Veuillez fournir un lieu...',
+                                    },
+                                ]}
+                            >
+                                {loadingData ? <Skeleton.Input active={true} /> : <Input placeholder="Saisir le lieu" />}
+                            </Form.Item>
+                        </Col>
 
-                    <Col xs={24} md={8}>
-                        <Form.Item
-                            name="numero_ordre"
-                            label="Numéro d'ordre"
-                            rules={[
-                                {
-                                    required: true,
-                                    message: 'Veuillez fournir un Numéro d ordre... ',
-                                },
-                            ]}
-                        >
-                                {loadingData ? <Skeleton.Input active={true} /> : <Input placeholder="Numéro d'ordre (optionnel)" />}
-                        </Form.Item>
-                    </Col>
+                        <Col xs={24} md={8}>
+                            <Form.Item
+                                name="date"
+                                label="Date"
+                                rules={[
+                                    {
+                                        required: true,
+                                        message: 'Veuillez fournir une date... ',
+                                    },
+                                ]}
+                            >
+                                    {loadingData ? <Skeleton.Input active={true} /> : <DatePicker style={{width:'100%'}} />}
+                            </Form.Item>
+                        </Col>
+
+                        <Col xs={24} md={8}>
+                            <Form.Item
+                                name="heure"
+                                label="Heure"
+                                rules={[
+                                    {
+                                        required: true,
+                                        message: 'Veuillez fournir une heure... ',
+                                    }
+                                ]}
+                            >
+                                    {loadingData ? <Skeleton.Input active={true} /> : <DatePicker style={{width:'100%'}} />}
+                            </Form.Item>
+                        </Col>
+
+                        <Col xs={24} md={8}>
+                            <Form.Item
+                                name="id_marque"
+                                label="Marque"
+                                rules={[
+                                    {
+                                        required: true,
+                                        message: 'Veuillez fournir une marque...',
+                                    },
+                                ]}
+                            >
+                                <Select placeholder="Choisir la marque">
+                                    <Option value="1">Marque 1</Option>
+                                    <Option value="2">Marque 2</Option>
+                                </Select>
+                            </Form.Item>
+                        </Col>
+                    </Row>
                 </Form>
             </div>
         </div>
