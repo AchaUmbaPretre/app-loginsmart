9273fc1e5fe4ab9a6766fa5289907bfc0b72825b
AchaUmbaPretre <achandambi@gmail.com>
2024-12-23 12:34:13 +0100

    commit

--- src/pages/carburant/carburantBoard/CarburantBoard.jsx
+++ src/pages/carburant/carburantBoard/CarburantBoard.jsx
@@ -1,41 +1,45 @@
 import { Divider, Table } from 'antd'
 import './carburantBoard.scss'
+import { useState } from 'react';
 
 const CarburantBoard = () => {
+  const [data, setData] = useState([]);
 
     const columns = [
         { title: '#', dataIndex: 'id', key: 'id', render: (_, __, index) => index + 1, width: "5%" },
         {
-          title: 'Name',
-          dataIndex: 'name',
-          key: 'name',
+          title: 'N°',
+          dataIndex: 'num',
+          key: 'num',
         },
         {
-          title: 'Age',
-          dataIndex: 'age',
-          key: 'age',
+          title: 'Immatriculation',
+          dataIndex: 'immatriculation',
+          key: 'immatriculation',
         },
         {
-          title: 'Address',
-          dataIndex: 'address',
-          key: 'address',
+          title: 'Qte',
+          dataIndex: 'qte',
+          key: 'qte',
+        },
+        {
+          title: 'Km',
+          dataIndex: 'km',
+          key: 'km',
+        },
+        {
+          title: 'Date',
+          dataIndex: 'date',
+          key: 'date',
+        },
+        {
+          title: 'User',
+          dataIndex: 'user',
+          key: 'user',
         },
       ];
 
-      const dataSource = [
-        {
-          key: '1',
-          name: 'Mike',
-          age: 32,
-          address: '10 Downing Street',
-        },
-        {
-          key: '2',
-          name: 'John',
-          age: 42,
-          address: '10 Downing Street',
-        },
-      ];
+
       const onChange = (pagination, filters, sorter, extra) => {
         console.log('params', pagination, filters, sorter, extra);
       };
@@ -46,12 +50,14 @@ const CarburantBoard = () => {
             <div className="carburantBord-wrapper">
                 <div className="carburantBord_top">
                     <h2 className="carburantBord-h2">Tableau de bord</h2>
-                    <Table columns={columns} size="small" dataSource={dataSource} onChange={onChange} />
+                    <Table columns={columns} size="small" dataSource={data} onChange={onChange} />
                 </div>
+
                 <Divider className='title_row'></Divider>
+                
                 <div className="carburantBord_bottom">
                     <h2 className="carburantBord-h2">SUZUKI GRAND VITARA</h2>
-                    <Table columns={columns} size="small" dataSource={dataSource} onChange={onChange} />
+                    <Table columns={columns} size="small" dataSource={data} onChange={onChange} />
                 </div>
             </div>
         </div>
