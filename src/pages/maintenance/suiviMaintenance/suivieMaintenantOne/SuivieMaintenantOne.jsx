20d4ab4e2f7847d1054139db837f41e0926279fb
AchaUmbaPretre <achandambi@gmail.com>
2024-12-26 17:03:29 +0100

    commit

--- src/pages/maintenance/suiviMaintenance/suivieMaintenantOne/SuivieMaintenantOne.jsx
+++ src/pages/maintenance/suiviMaintenance/suivieMaintenantOne/SuivieMaintenantOne.jsx
@@ -1,5 +1,5 @@
 import { Badge, Breadcrumb, Button,Menu, Input, Modal,Dropdown, Popconfirm,Tag, Space, Table, Tooltip } from 'antd';
-import { PlusCircleOutlined,EyeOutlined,MoreOutlined,FileTextOutlined,CarOutlined,CalendarOutlined,ToolOutlined,ShopOutlined,SyncOutlined,CheckCircleOutlined,DeleteOutlined,EditOutlined, SearchOutlined, FilterOutlined } from '@ant-design/icons';
+import { PlusCircleOutlined,EyeOutlined,MoreOutlined,CarOutlined,CalendarOutlined,ToolOutlined,ShopOutlined,SyncOutlined,CheckCircleOutlined,DeleteOutlined,EditOutlined, SearchOutlined, FilterOutlined } from '@ant-design/icons';
 import { useEffect, useState } from 'react';
 import Maintenance_form from './maintenance_form/Maintenance_form';
 import maintenanceService from '../../services/maintenance.service';
