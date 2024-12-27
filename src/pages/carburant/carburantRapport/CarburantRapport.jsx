703ccf14b2cac1a705f45c9f405b620b57ded167
AchaUmbaPretre <achandambi@gmail.com>
2024-12-27 15:29:54 +0100

    commit

--- src/pages/carburant/carburantRapport/CarburantRapport.jsx
+++ src/pages/carburant/carburantRapport/CarburantRapport.jsx
@@ -21,7 +21,7 @@ const CarburantRapport = () => {
         <div className="carburantRapport">
             <div className="carburantRapport_wrapper">
                 <div className="carburantRapport_row_title">
-                    <h2 className="carburant_h2">Rapport consomation carburant par vehicule (sur les 360 dernier jours )</h2>
+                    <h2 className="carburant_h2">RAPPORT DE CONSOMMATION DE CARBURANT PAR VÉHICULE (SUR LES 360 DERNIERS JOURS)</h2>
                 </div>
                 <div className="carburantRapport_top">
 
