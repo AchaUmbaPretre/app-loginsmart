507a71d98f5e0b8df9c261a46435332e09c74f62
AchaUmbaPretre <achandambi@gmail.com>
2025-01-31 14:00:02 +0100

    commit

--- src/pages/carburant/carburantRapport/carburantTabDetailSites/carburantTabDetailSiteChart/CarburantTabDetailSiteChart.jsx
+++ src/pages/carburant/carburantRapport/carburantTabDetailSites/carburantTabDetailSiteChart/CarburantTabDetailSiteChart.jsx
@@ -1,8 +1,30 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import carburantService from '../../../../../services/carburant.service';
 
 const CarburantTabDetailSiteChart = ({selectedVehicles}) => {
-    console.log(selectedVehicles)
+    const [loading, setLoading] = useState(false);
+    const [data, setData] = useState([]);
+    const scroll = { x: 400 };
     
+    const fetchData = async () =>{
+        try {
+          setLoading(true);
+          const [carburantData] = await Promise.all([
+            carburantService.getCarburantRapportDetailSiteSelect(selectedVehicles)
+          ])
+          setData(carburantData)
+    
+        } catch (error) {
+          console.log(error)
+        } finally {
+          setLoading(false);
+        }
+      }
+    
+      useEffect(()=> {
+        fetchData()
+      }, [selectedVehicles])
+
   return (
     <div>CarburantTabDetailSiteChart</div>
   )
